4f7f853da03b74b393a62cca6801d822c79caaae
Tran Nguyen <tranhnguyen@cpp.edu>
2022-03-02 17:39:59 -0800

    Source Code

--- numberGuess.c
+++ numberGuess.c
@@ -37,3 +37,108 @@ result loop
 
 
 */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+enum gameResult{LOST, WIN};
+
+int main(){
+    const int MAX_GAME = 1000;
+    const int MAX_VALUE = 10000;
+
+    int condition = 1, maxCond = 1;
+    int number,  max = 10,inputMax, totalGame = 0, selection, numOfGuesses, correct; 
+    char guessNumber[100];
+    
+    int result[MAX_GAME];
+    int totalGuesses[MAX_GAME];
+    
+    
+    const char *gameResult[] = {"Lost","Win"};
+    const char menu[] = "Press 1 to play a game\nPress 2 to change the max number\nPress 3 to quit\n";
+
+    time_t t;
+    enum gameResult game;
+
+    while(condition == 1){
+        printf("\n%s\nYour selection: ",menu);
+        scanf("%d", &selection);
+        
+        if(selection == 1){
+            srand((unsigned) time(&t));
+            number = (rand () % max) + 1; 
+            numOfGuesses = 0;
+            game = LOST;
+            correct = 0;
+            while(correct != 1){
+                printf("Enter a number: ");
+                scanf("%s", guessNumber);
+                
+
+                if(*guessNumber == 'q'){
+                    break;
+                }
+                else{
+                    if (atoi(guessNumber) == number){
+                        game = WIN;
+                        printf("=> Correct! You have won!\n");
+                        correct = 1;
+                    }
+                    else if (atoi(guessNumber) > number){
+                        printf("Too high\n");
+
+                    }
+                    else if (atoi(guessNumber) < number){
+                        printf("Too low\n");
+                    }
+                }
+                numOfGuesses++;
+            }
+            result[totalGame] = game;
+            totalGuesses[totalGame]=numOfGuesses;
+            totalGame++;
+
+
+        }
+        else if(selection == 2){
+            maxCond = 1;
+            printf("The max value you can set is: %d\n",MAX_VALUE);
+            while(maxCond == 1){
+                
+                printf("Enter a new max value: ");
+                scanf("%d",&inputMax);
+                if(inputMax < 0){
+                    printf("Max value cannot be negative!\n");
+                }
+                else if (inputMax > MAX_VALUE){
+                    printf("Max value must be less than %d\n",MAX_VALUE);   
+                }
+                else{
+                    max = inputMax;
+                    maxCond = 0;
+                }
+            }
+
+
+        }
+        else if (selection == 3){
+            break;
+        }
+        else{
+            printf("Invalid Selection\n");
+            
+        }
+        printf("----------------------------------\n");
+
+    }
+    printf("\nThank you for playing the Number Guessing Game!\n\n");
+    printf("Game\tResult\tGuesses\n");
+    for (int game = 0; game < totalGame; game++){
+
+        printf("%d\t %s\t  %d\n", game, gameResult[result[game]], totalGuesses[game]);
+       
+    }
+    return(0);
+}
+
