12c4bb8715683a73211c3f8548839146dad22cfe
Tran Nguyen <tranhnguyen@cpp.edu>
2022-03-02 17:39:20 -0800

    Outline

---
+++ numberGuess.c
@@ -0,0 +1,39 @@
+/*
+Include headers
+
+Declare and init: max number of game, max value that user can choose
+Declare variables: number, guess number, input, conditions, guesses, total game
+Declare Arrays: keep track of results and guesses
+
+Declare and init the menu
+
+loop menu:
+    Print out the menu 
+    Press 1 to play a game 
+    Press 2 to change the max number 
+    Press 3 to quit
+
+    prompt for user selection
+
+    1: randomize the number, reset number of guesses
+       prompt and validate the guessing number
+       allows 'q' as exit
+
+       Init random number generator
+            Too high
+            Too low
+            Correct and save the result
+
+
+    2: prompt for max and validate the input
+
+    3: exit the loop menu
+
+output the thank you statement
+
+result loop
+    Print the results and number of guesses
+
+
+
+*/
